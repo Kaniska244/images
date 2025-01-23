26fba47378acbcc63434a70e663b103029f774ad
Kaniska <kaniska244@github.com>
2025-01-23 21:30:56 +0530

    Update install.sh, to test

--- src/universal/.devcontainer/local-features/setup-user/install.sh
+++ src/universal/.devcontainer/local-features/setup-user/install.sh
@@ -103,6 +103,33 @@ chown -R ${USERNAME}:oryx ${OPT_DIR}
 chmod -R g+r+w "${OPT_DIR}"
 find "${OPT_DIR}" -type d | xargs -n 1 chmod g+s
 
+echo "Workspace_Check"
+cd /workspaces
+pwd
+ls -lha
+cd images
+pwd
+ls -lha
+cd src
+pwd
+ls -lha
+cd universal
+pwd
+ls -lha
+cd test-project
+pwd
+ls -lha
+cd sample
+pwd
+ls -lha
+cd java
+pwd
+ls -lha
+cd ..
+cd python
+pwd
+ls -lha
+
 echo "After_Third"
 echo ${USERNAME}
 cat /etc/passwd
