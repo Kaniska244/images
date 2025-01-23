2cd6ba6ddd041ffb15757ad0ac2ab35a7ff530b9
Kaniska <kaniska244@github.com>
2025-01-23 14:03:02 +0530

    Update puppeteer.js, sandbox change

--- src/universal/test-project/puppeteer.js
+++ src/universal/test-project/puppeteer.js
@@ -7,8 +7,8 @@ const { exit } = require('process');
         "args": ["--no-sandbox"]
     })
     //const browser = await puppeteer.launch();
-    const browser = await puppeteer.launch(  {
-  args: ['--no-sandbox', '--disable-setuid-sandbox']
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
   
     const page = await browser.newPage();
