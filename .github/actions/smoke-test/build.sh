2fe0068723fd3f1b2da95a58ee3e045c589084d8
Kaniska <kaniska244@github.com>
2025-02-21 18:43:41 +0530

    Update build.sh, test

--- .github/actions/smoke-test/build.sh
+++ .github/actions/smoke-test/build.sh
@@ -9,10 +9,12 @@ npm install -g @devcontainers/cli
 
 echo "(*) Building image - ${IMAGE}"
 id_label="test-container=${IMAGE}"
+id_image="universal-test-image"
 #devcontainer up --id-label ${id_label} --workspace-folder "src/${IMAGE}/"
 if [ $IMAGE == "universal" ]; then
-    devcontainer up --id-label ${id_label} --workspace-folder "src/${IMAGE}/" --build-no-cache
-
+    devcontainer build --image-name ${id_image} --workspace-folder "src/${IMAGE}/"
+    
+    devcontainer up --id-label ${id_label} --workspace-folder "src/${IMAGE}/"
 else
     devcontainer up --id-label ${id_label} --workspace-folder "src/${IMAGE}/"
 fi
